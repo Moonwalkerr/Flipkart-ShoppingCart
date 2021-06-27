b00c71b658a255c2c954d42885248c23274e245d
Moonwalker <abhisk.mhra@gmail.com>
2021-06-27 10:57:27 +0530

    add to cart button Wip

--- src/UI/prodList/product.js
+++ src/UI/prodList/product.js
@@ -1,5 +1,6 @@
+import { IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 // stylings for the Navbar via material ui stylings
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -7,7 +8,7 @@ const useStyles = makeStyles((theme) => ({
     padding: 15,
     display: "flex",
     flexDirection: "column",
-    height: "35vh",
+    height: "36vh",
     width: "24vw",
     boxShadow: "1px 1px 3px 3px rgba(0,0,0,0.1)",
   },
@@ -15,6 +16,32 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(1.2),
     objectFit: "contain",
     height: "25vh",
+  },
+  prodName: {
+    fontWeight: "400",
+  },
+  productDetails: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 6,
+  },
+  productPrice: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  cart: {
+    fontSize: 14,
+    padding: 6,
+    letterSpacing: 1,
+    borderRadius: 10,
+    fontWeight: "bold",
+    backgroundColor: "#FF9F00",
+    color: "#fafafafa",
+    "&:hover": {
+      backgroundColor: "#FF9F00",
+      color: "#fafafafa",
+    },
   },
 }));
 
@@ -25,8 +52,13 @@ const ProductCard = ({ product }) => {
   return (
     <div className={classes.root}>
       <img className={classes.productImg} src={product.imageURL} alt="" />
-      <h3>{product.name}</h3>
-      <span className="price"> ₹ {product.price}</span>
+      <p className={classes.prodName}>{product.name}</p>
+      <div className={classes.productDetails}>
+        <span className={classes.productPrice}> ₹ {product.price}</span>
+        <IconButton className={classes.cart}>
+          <ShoppingCartIcon /> <span>Add to cart</span>
+        </IconButton>
+      </div>
     </div>
   );
 };
