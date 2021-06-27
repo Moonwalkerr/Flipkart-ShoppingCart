e6f67856f5ff849638fdac45a6fbbc6aaa21860e
Moonwalker <abhisk.mhra@gmail.com>
2021-06-27 10:39:36 +0530

    Product List grid Creation phase completed

--- src/UI/dashboard/index.js
+++ src/UI/dashboard/index.js
@@ -1,9 +1,11 @@
 import Nav from "../nav";
+import ProdListGrid from "../prodList";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <Nav />
+      <ProdListGrid />
     </div>
   );
 };
--- src/UI/prodList/index.js
+++ src/UI/prodList/index.js
@@ -1,17 +1,12 @@
 import { makeStyles } from "@material-ui/core/styles";
-import ProductCard from "./productCard";
+import ProductCard from "./product";
 import { useContext } from "react";
 import { AppContext } from "../../context/context";
 
 // stylings for the Navbar via material ui stylings
 const useStyles = makeStyles((theme) => ({
-  dashboard: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
-  },
   root: {
-    flex: 0.95,
+    width: "100%",
     margin: theme.spacing(1),
     display: "grid",
     gridTemplateColumns: "repeat(3,1fr)",
@@ -29,12 +24,10 @@ const ProdListGrid = () => {
   const classes = useStyles();
   const displayData = useContext(AppContext).displayData[0];
   return (
-    <div className={classes.dashboard}>
-      <div className={classes.root}>
-        {displayData.map((product) => {
-          return <ProductCard product={product} />;
-        })}
-      </div>
+    <div className={classes.root}>
+      {displayData.map((product) => {
+        return <ProductCard product={product} />;
+      })}
     </div>
   );
 };
